4f2d9f7984e139f16389474fb1b412241b06ea0a
cht <cht@meitu.com>
2017-09-15 23:54:32 +0800

    push all words's json

---
+++ src/app.groovy
@@ -0,0 +1,29 @@
+def baseDir = './../res/'
+def fileWordsList = 'all.english.word.69903.log'
+
+def wordUrl = 'http://www.iciba.com/index.php?a=getWordMean&c=search&word='
+
+def inputFile = new File(baseDir + fileWordsList)
+
+def count = 0
+
+inputFile.eachLine {word ->
+//    println(word)
+    String fullUrl = wordUrl + word
+    URL requestUrl = new URL (fullUrl)
+    String wordJson = requestUrl.getText()
+    def outputFile = new File("${baseDir}words/${word}.json")
+    if (!outputFile.getParentFile().exists()) {
+        outputFile.getParentFile().mkdirs()
+    }
+    if (outputFile.exists()) {
+        outputFile.delete()
+    }
+    outputFile.createNewFile()
+
+    wordJson = wordJson.replace("\\/", "/")
+
+    wordJson = AsciiUtil.ascii2Native(wordJson)
+    outputFile << wordJson
+    println("write : ${word}")
+}
